c588f59c304d6c68ce8e1dee8928c02036fcfc23
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 14:55:29 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,8 +15,8 @@ function frameIt() {
 
 function pressIt() {
   $('document').on('keydown', function(key) {
-    if(key.which == 83){
-      alert('s was pressed');
+    if(key.which == 71){
+      alert('G was pressed');
     }
   });
 }
